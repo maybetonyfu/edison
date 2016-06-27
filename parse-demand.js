dd82e0e3d88f1e65b1da767528c0b21f299ec343
Tony Fu <tonyfu.dev@gmail.com>
2016-06-27 08:45:22 +0000

    get demand and parse demand

---
+++ parse-demand.js
@@ -0,0 +1,60 @@
+var fs = require('fs')
+var csv = require('csv')
+var moment = require('moment')
+
+var states = {
+    'NSW1': {
+        common_name: 'NSW',
+        nem_name: 'NSW1'
+    },
+    'QLD1': {
+        common_name: 'QLD',
+        nem_name: 'QLD1'
+    },
+    'SA1': {
+        common_name: 'SA',
+        nem_name: 'SA1'
+    },
+    'TAS1': {
+        common_name: 'TAS',
+        nem_name: 'TAS1'
+    },
+    'VIC1': {
+        common_name: 'VIC',
+        nem_name: 'VIC1'
+    }
+}
+
+fs.readFile('test.csv', 'utf8', function (err, data) {
+
+    if (err) {
+        return console.log(err)
+    }
+
+    var demand_data = data
+
+    var options = {
+        columns: true
+    }
+
+    csv.parse(demand_data, options, transform_demand_data)
+
+})
+
+function transform_demand_data(err, data) {
+
+    if (err) {
+        console.log(err)
+    }
+
+    var transformed_data = data.map(function (datum) {
+        return {
+            region: states[datum.REGION].common_name,
+            settlement_date: moment.utc(datum.SETTLEMENTDATE, 'YYYY/MM/DD HH:mm:ss', true).toISOString(),
+            total_demand: datum.TOTALDEMAND,
+            price: datum.RRP
+        }
+    })
+
+    console.log(transformed_data)
+}
