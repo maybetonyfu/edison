0ec7a2dee0691d8d623919a4359f3d497b003132
Tony Fu <tonyf@gorapid.com.au>
2016-06-28 14:33:48 +1000

    Add eslint rules

--- src/get-demand.js
+++ src/get-demand.js
@@ -2,6 +2,9 @@ import http from 'http'
 import fs from 'fs'
 
 let file = fs.createWriteStream("demand_data/DATA201112_NSW1_NEW.csv");
-let request = http.get("http://www.nemweb.com.au/mms.GRAPHS/data/DATA201112_NSW1.csv", function(response) {
-  response.pipe(file);
+
+http.get("http://www.nemweb.com.au/mms.GRAPHS/data/DATA201112_NSW1.csv", response => {
+
+    response.pipe(file);
+
 });
