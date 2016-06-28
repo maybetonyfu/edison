27a3889fc9ca60722678746cc67a9017b6e63007
Tony Fu <tonyf@gorapid.com.au>
2016-06-28 18:30:59 +1000

    decompose demand url

--- src/get-demand.js
+++ src/get-demand.js
@@ -1,9 +1,23 @@
 import http from "http"
 import fs from "fs"
 
-let file = fs.createWriteStream("../data/demand/test/test.csv")
 
-http.get("http://www.nemweb.com.au/mms.GRAPHS/data/DATA201112_NSW1.csv", (response) => {
+
+let nem = "http://www.nemweb.com.au"
+
+let demand_path = "/mms.GRAPHS/data/"
+
+let month = "12"
+
+let year = "2011"
+
+let state = "NSW"
+
+let file = fs.createWriteStream(`../data/demand/${state}/${year}-${month}.csv`)
+
+// "http://www.nemweb.com.au/mms.GRAPHS/data/DATA201112_NSW1.csv"
+
+http.get(`${nem}${demand_path}DATA${year}${month}_${state}1.csv`, (response) => {
 
     response.pipe(file)
 
