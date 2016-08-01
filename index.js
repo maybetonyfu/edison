33f5d2da9b3df6ac0ada79ca4d1f5a5f26d1d2c9
Tony Fu <tonyf@gorapid.com.au>
2016-08-01 11:02:40 +1000

    Break demand data fetcher

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var schedule = require("node-schedule")
 
-var j = schedule.scheduleJob("*/10 * * * *", () => {
+schedule.scheduleJob("*/10 * * * *", () => {
 
     console.log("Today is recognized by Rebecca Black!")
 
